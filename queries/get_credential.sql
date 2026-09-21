4ab98002a1e480774c13706e9a5cc4d92d7eb8bb
Nathan Blair <me@nathanblair.rocks>
2026-09-20 23:02:40 -0400

    fix: glob it

--- queries/get_credential.sql
+++ queries/get_credential.sql
@@ -1,5 +1,4 @@
 -- name: GetCredential :one
-SELECT id, principal_id, sign_count, uv_initialized, backup_state,
-       attestation_object, attestation_client_data_json, transports, rp_id, current_login_challenge
+SELECT *
   FROM credential
  WHERE id = $1;
